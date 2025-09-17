2cb36f6c9f1b48fb14febdc5ff3bb52b65084289
pixel <squirreljedi777@gmail.com>
2025-09-17 16:30:15 -0500

    Create emote.ts

---
+++ subattleroyale/server/src/objects/emote.ts
@@ -0,0 +1,14 @@
+
+import type { AllowedEmoteSources } from "@common/packets/inputPacket";
+import { type Player } from "./player";
+
+export class Emote {
+    readonly playerID: number;
+
+    constructor(
+        readonly definition: AllowedEmoteSources,
+        readonly player: Player
+    ) {
+        this.playerID = player.id;
+    }
+}
